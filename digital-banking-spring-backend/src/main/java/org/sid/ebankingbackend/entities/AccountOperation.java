200c2c007154e2a98b8849835465501991c98d18
Khalid ECH-CHERQAOUI <kechcherqaoui@xperlean.com>
2023-08-21 12:56:35 +0100

    first commit

---
+++ digital-banking-spring-backend/src/main/java/org/sid/ebankingbackend/entities/AccountOperation.java
@@ -0,0 +1,32 @@
+package org.sid.ebankingbackend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.sid.ebankingbackend.enums.OperationType;
+
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class AccountOperation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    @Enumerated(EnumType.STRING)
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+    private String description;
+}
+
---
+++ digital-banking-spring-backend/src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
@@ -0,0 +1,36 @@
+package org.sid.ebankingbackend.entities;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE",length = 4)
+@Data @NoArgsConstructor @AllArgsConstructor
+public abstract class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private Date createdAt;
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+
+    @OneToMany(mappedBy = "bankAccount",fetch = FetchType.LAZY)
+    private List<AccountOperation> accountOperations;
+}
